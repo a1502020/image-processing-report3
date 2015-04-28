240540d2e97d327659d9f70170085d57e674bd92
Chisaka <a1502020@sendai-nct.jp>
2015-04-28 11:30:27 +0900

    グレースケール画像を表すクラスを追加

---
+++ report3/GlayImage.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ImageProcessing
+{
+    class GlayImage
+    {
+        public GlayImage()
+        {
+        }
+
+        public GlayImage(string path)
+        {
+            Read(path);
+        }
+
+        public void Read(string path)
+        {
+            var state = State.Header;
+            var type = "P2";
+            var y = 0;
+            using (var reader = new StreamReader(path))
+            {
+                while (reader.Peek() >= 0 && state != State.Done)
+                {
+                    if (state == State.Header)
+                    {
+                        type = reader.ReadLine();
+                        if (type != "P2" && type != "P5")
+                        {
+                            throw new NotImplementedException("P2, P5 形式以外は未対応です。");
+                        }
+                        state = State.Size;
+                    }
+                    else if (state == State.Size)
+                    {
+                        var line = reader.ReadLine();
+                        if (line.StartsWith("#"))
+                        {
+                            continue;
+                        }
+                        var values = line.Split(new char[] { ' ' }).Select(str => int.Parse(str)).ToList();
+                        Size = new Size(values[0], values[1]);
+                        state = State.MaxValue;
+                    }
+                    else if (state == State.MaxValue)
+                    {
+                        var line = reader.ReadLine();
+                        if (line.StartsWith("#"))
+                        {
+                            continue;
+                        }
+                        if (line != "255")
+                        {
+                            throw new NotImplementedException("256階調以外は未対応です。");
+                        }
+                        state = (type == "P2") ? State.ContentP2 : State.ContentP5;
+                    }
+                    else if (state == State.ContentP2)
+                    {
+                        var values = reader.ReadLine()
+                            .Split(new char[] { ' ' })
+                            .Select(str => int.Parse(str)).ToList();
+                        for (var x = 0; x < Size.Width; ++x)
+                        {
+                            image[x][y] = values[x];
+                        }
+                        ++y;
+                    }
+                    else if (state == State.ContentP5)
+                    {
+                        for (var x = 0; x < Size.Width; ++x)
+                        {
+                            image[x][y] = reader.Read();
+                        }
+                        ++y;
+                    }
+                }
+            }
+        }
+
+        public void Write(string path, string type = "P5")
+        {
+            if (type != "P2" && type != "P5")
+            {
+                throw new NotImplementedException("P2, P5 形式以外は未対応です。");
+            }
+
+            if (type == "P2")
+            {
+                using (var writer = new StreamWriter(path))
+                {
+                    for (var y = 0; y < Size.Height; ++y)
+                    {
+                        for (var x = 0; x < Size.Width; ++x)
+                        {
+                            if (x != 0)
+                            {
+                                writer.Write(" ");
+                            }
+                            writer.Write("{0}", image[x][y]);
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                using (var writer = new BinaryWriter(File.OpenWrite(path)))
+                {
+                    writer.Write(type + Environment.NewLine);
+                    writer.Write(String.Format("{0} {1}" + Environment.NewLine, Size.Width, Size.Height));
+                    writer.Write(String.Format("{0}" + Environment.NewLine, 255));
+                    for (var y = 0; y < Size.Height; ++y)
+                    {
+                        for (var x = 0; x < Size.Width; ++x)
+                        {
+                            writer.Write((byte)image[x][y]);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 画像の大きさ
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                var newImg = new List<List<int>>();
+                for (var y = 0; y < value.Height; ++y)
+                {
+                    var line = new List<int>();
+                    for (var x = 0; x < value.Width; ++x)
+                    {
+                        if (x < size.Width && y < size.Height)
+                        {
+                            line.Add(image[x][y]);
+                        }
+                        else
+                        {
+                            line.Add(0);
+                        }
+                    }
+                    newImg.Add(line);
+                }
+                image = newImg;
+                size = value;
+            }
+        }
+
+        /// <summary>
+        /// 画素
+        /// </summary>
+        /// <param name="x">X 座標</param>
+        /// <param name="y">Y 座標</param>
+        /// <returns>階調値</returns>
+        public int this[int x, int y]
+        {
+            get
+            {
+                return image[x][y];
+            }
+            set
+            {
+                image[x][y] = value;
+            }
+        }
+
+        private Size size = new Size(0, 0);
+        private List<List<int>> image = new List<List<int>>();
+
+        enum State
+        {
+            Header,
+            Size,
+            MaxValue,
+            ContentP2,
+            ContentP5,
+            Done,
+        }
+    }
+}
